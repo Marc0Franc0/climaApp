d79699048133ba27a5e08d570a63aae8e28e1285
Marc0Franc0 <marcofranco340@gmail.com>
2023-01-05 16:04:42 -0300

    Integración de SweetAlert2 para las alertas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { JsonpClientBackend } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ApiClimaService } from './services/api-clima.service';
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,15 +10,25 @@ import { ApiClimaService } from './services/api-clima.service';
 export class AppComponent {
   title = 'climaApp';
   clima:any;
+
   constructor(private climaService:ApiClimaService){}
 
   submitLocation(cityName:HTMLInputElement,countryCode:HTMLInputElement){
     if(cityName.value && countryCode.value){
          this.getClima(cityName.value,countryCode.value);
+
           cityName.value='';
     countryCode.value='';
     this.clima=null;
-    }else{alert("Faltan datos a ingresar")}
+    }else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Algo salió mal',
+        text: 'Faltan datos a ingresar',
+        position: 'top',
+
+      })
+     }
 
 
     cityName.focus();
@@ -25,7 +36,18 @@ export class AppComponent {
     }
 
     getClima(cityName:string,countryCode:string){
-this.climaService.obtenerClima(cityName,countryCode).subscribe(res=>{console.log(res),this.clima=res})
+this.climaService.obtenerClima(cityName,countryCode).subscribe(res=>{
+  console.log(res),this.clima=res},err=>{console.log(err)
+  if(err.message!=""){
+    Swal.fire({
+      icon: 'warning',
+      title: 'Algo salió mal',
+      text: 'Verifique los datos ingresados ',
+      position: 'top',
+
+    })
+  }
+  })
 
     }
 
